3551c44b419825b2765b34695653305fb40f6a5f
Shalini-Barai <115135802+Shalini-Barai@users.noreply.github.com>
2023-04-03 12:15:32 +0530

    sum1

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,14 @@
-import React,{useState} from 'react'
+import React from 'react'
 import './../styles/App.css';
+import SumCalculator from './SumCalculator';
 
 const App = () => {
-  const [numbers, setNumbers] = useState([]);
-  const [sum, setSum] = useState(0);
-
-  const handleChange = async (event) => {
-    const value = event.target.value;
-    // Parse the input value as a number
-    const number = parseFloat(value);
-
-    // Check if the input is a valid number
-    if (!isNaN(number)) {
-      // Add the number to the array of numbers
-      setNumbers([...numbers, number]);
-
-      // Calculate the sum asynchronously to prevent UI freezes
-      await new Promise((resolve) => setTimeout(resolve, 0));
-      setSum(sum + number);
-    }
-  };
+ 
   return (
     <div>
         {/* Do not remove the main div */}
-        <h1>Sum Calculator</h1>
-        <input type="number" onChange={handleChange} />
-      <p>Sum: {sum}</p>
         
+        <SumCalculator/>
     </div>
   )
 }
---
+++ src/components/SumCalculator.js
@@ -0,0 +1,34 @@
+import React,{useState} from 'react'
+
+
+const SumCalculator = () => {
+  const [numbers, setNumbers] = useState([]);
+  const [sum, setSum] = useState(0);
+
+  const handleChange = async (event) => {
+    const value = event.target.value;
+    // Parse the input value as a number
+    const number = parseFloat(value);
+
+    // Check if the input is a valid number
+    if (!isNaN(number)) {
+      // Add the number to the array of numbers
+      setNumbers([...numbers, number]);
+
+      // Calculate the sum asynchronously to prevent UI freezes
+      await new Promise((resolve) => setTimeout(resolve, 0));
+      setSum(sum + number);
+    }
+  };
+  return (
+    <div>
+        {/* Do not remove the main div */}
+        <h1>Sum Calculator</h1>
+        <input type="number" onChange={handleChange} />
+      <p>Sum: {sum}</p>
+        
+    </div>
+  )
+}
+
+export default SumCalculator
